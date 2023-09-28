1961b83d6daba32ebd37e17c013bf84ed103b535
ghamlet <114679713+ghamlet@users.noreply.github.com>
2023-09-28 15:41:05 +0400

    Add files via upload

---
+++ etht.pas
@@ -0,0 +1,20 @@
+﻿var
+  s, newstr:string;
+  c:=1;
+  
+  begin
+    s:= 'привет это диктор канала мастерская настроения';
+    
+    for var i:=1 to length(s) do
+      begin
+        
+        if s[i] <> ' ' then
+          newstr += s[i]
+        else c+=1;
+        
+      end;
+      
+   println(c); 
+   println(newstr);
+    
+  end.
