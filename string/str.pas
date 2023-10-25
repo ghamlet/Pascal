e5a2ef3fe3a780db2bce2109e83db8caed4f2dae
ghamlet <114679713+ghamlet@users.noreply.github.com>
2023-10-25 19:19:16 +0300

    Add files via upload

---
+++ string/str.pas
@@ -0,0 +1,20 @@
+﻿var s, pat: string;
+n:integer;
+
+begin
+  n:= 80;
+  pat:= '21';
+  
+  while s <> pat do
+  begin
+    n+=1;
+    s:='1'* n;
+    
+    while (pos('111', s) > 0) do
+      begin
+        s:=s.Replace('111', '2', 1);
+        s:=s.Replace('222', '1', 1);
+      end;
+  end;
+   writeln(n);
+end.
