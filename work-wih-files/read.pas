0a0b320f696f79db398e299f9dac257aaf49af9b
Dmitry Mayorov <114679713+ghamlet@users.noreply.github.com>
2024-04-03 22:35:13 +0300

    Add files via upload

---
+++ work-wih-files/read.pas
@@ -0,0 +1,32 @@
+﻿uses  GraphABC;
+ 
+var
+  f:text;
+  first_el,second_el, N, sum,sr,h: integer;
+  
+BEGIN
+  N:=266*2; //если дано количество строк
+  assign(f, 'run.txt');
+  reset(f); //чтение файла
+  
+  for var i:=1 to N do //while not eof(f) do  если неизвестно количество строк
+    begin
+      read(f, first_el); // если надо взаимодействовать с парами чисел, то читаем за раз две переменные: read(f, first_el, second_el)
+//      print(first_el);
+      sum += first_el;
+      
+      
+    end;
+//println(' ');
+sr:=round(sum / N);
+//
+//writeln('The average of the numbers is: ',sr );
+
+h:=round(sr*20);
+rectangle(920,10, 1000,h);
+floodfill(950, 20, clBlue);
+circle(950, 20, 5);
+
+close(f);
+ END.
+ 
---
+++ work-wih-files/write-to-file.pas
@@ -0,0 +1,25 @@
+﻿// запись в файл рандомных чисел
+var
+  f:text;
+  n,num1, num2: integer;
+  
+BEGIN
+  assign(f, 'run.txt');
+  rewrite(f);
+  
+  n:=random(1,300);
+  print(n);
+  for var i:=1 to n do
+    begin
+      num1:=random(-1000, 1000);
+      num2:=random(-1000, 1000);
+      //write(f, ' ', num1);
+       // writeln(f, num1, ' ', num2); //запись в столбец 
+        write(f, num1, ' ', num2, ' '); //нормальная запись в одну строку: позволяет легко считывать как по одному так и по 2 элемента
+    end;
+ 
+ close(f);
+ 
+ END.
+ 
+ 
